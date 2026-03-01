cdce31c081383845b0071a6efb6cac5ca6cfd70d
Arin Saynere <arinsaynere@gmail.com>
2026-03-01 23:27:27 +0530

    Dataframe

---
+++ dataframe.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = {
+    "Name": ["Max", "Rahul", "Neha","Ram","Sham"],
+    "Age": [22, 24, 23,25,26],
+    "City": ["Pune", "Mumbai", "Nagpur","Amravati","Hyderabad"]
+}
+
+df = pd.DataFrame(data)
+
+
+print(df)
+
+print(df.index)
+
+print(df.columns)
+
+print(df.dtypes)
+
+
+
+
+
