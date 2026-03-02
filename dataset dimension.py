156f17c82fbf9a4203c63ac0a7797b1d285cd939
Arin Saynere <arinsaynere@gmail.com>
2026-03-02 23:52:01 +0530

    Dataset Dimension

---
+++ dataset dimension.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+data = {
+    "Name": ["Max", "Rahul", "Neha","Ram","Sham"],
+    "Age": [22, 24, 23,25,26],
+    "City": ["Pune", "Mumbai", "Nagpur","Amravati","Hyderabad"]
+}
+
+df = pd.DataFrame(data)
+
+
+print(df)
+
+
+print(df.head(2))
+
+print(df.tail(2))
+
+print(df.size)
+
+print(df.shape)
+
+
+
+
+
